3798f47e944786a98404fef593457c425406a121
Qwizer <romario.sr@live.com>
2017-08-04 19:23:54 -0300

    Config.lua

---
+++ config.lua
@@ -0,0 +1,119 @@
+-- Combat settings
+-- NOTE: valid values for worldType are: "pvp", "no-pvp" and "pvp-enforced"
+worldType = "pvp"
+hotkeyAimbotEnabled = true
+protectionLevel = 8
+killsToRedSkull = 6
+killsToBlackSkull = 10
+pzLocked = 60 * 1000
+removeChargesFromRunes = true
+timeToDecreaseFrags = 24 * 60 * 60 * 1000
+whiteSkullTime = 15 * 60 * 1000
+stairJumpExhaustion = 2 * 1000
+experienceByKillingPlayers = false
+expFromPlayersLevelRange = 75
+
+-- Connection Config
+-- NOTE: maxPlayers set to 0 means no limit
+ip = "167.114.21.72"
+bindOnlyGlobalAddress = false
+loginProtocolPort = 7171
+gameProtocolPort = 7172
+statusProtocolPort = 7171
+maxPlayers = 2000
+motd = "Welcome To Tibia-Global! OpenServer - Prey System + Imbuiment System and More..."
+onePlayerOnlinePerAccount = true
+allowClones = false
+serverName = "Tibia-Global"
+statusTimeout = 5 * 1000
+replaceKickOnLogin = true
+maxPacketsPerSecond = 50
+enableLiveCasting = true
+liveCastPort = 7173
+
+-- Store in-Game Config
+coinPacketSize = 1
+coinImagesURL = "http://127.0.0.1/store/"
+
+-- PVP-Expert Config
+expertPvp = false
+
+-- Depot Limit
+freeDepotLimit = 2000
+premiumDepotLimit = 10000
+depotBoxes = 17
+
+-- Deaths
+-- NOTE: Leave deathLosePercent as -1 if you want to use the default
+-- death penalty formula. For the old formula, set it to 10. For
+-- no skill/experience loss, set it to 0.
+deathLosePercent = -1
+
+-- Houses
+-- NOTE: set housePriceEachSQM to -1 to disable the ingame buy house functionality
+housePriceEachSQM = 1000
+houseRentPeriod = "weekly"
+
+-- Item Usage
+timeBetweenActions = 200
+timeBetweenExActions = 1000
+
+-- Map
+-- NOTE: set mapName WITHOUT .otbm at the end
+mapName = "global"
+mapAuthor = "Cipsoft"
+
+-- Market
+marketOfferDuration = 30 * 24 * 60 * 60
+premiumToCreateMarketOffer = false
+checkExpiredMarketOffersEachMinutes = 60
+maxMarketOffersAtATimePerPlayer = 100
+
+-- MySQL
+mysqlHost = "127.0.0.1"
+mysqlUser = "root"
+mysqlPass = "123456"
+mysqlDatabase = "servidor"
+mysqlPort = 3306
+passwordType = "sha1"
+mysqlSock = ""
+
+-- Misc.
+allowChangeOutfit = true
+freePremium = true
+kickIdlePlayerAfterMinutes = 15
+maxMessageBuffer = 4
+emoteSpells = false
+classicEquipmentSlots = true
+allowWalkthrough = false
+
+-- Rates
+-- NOTE: rateExp is not used if you have enabled stages in data/XML/stages.xml
+rateExp = 1
+rateSkill = 30
+rateLoot = 4
+rateMagic = 8
+rateSpawn = 1
+
+-- Monsters
+deSpawnRange = 2
+deSpawnRadius = 50
+
+-- Stamina
+staminaSystem = true
+
+-- Scripts
+warnUnsafeScripts = true
+convertUnsafeScripts = true
+
+-- Startup
+-- NOTE: defaultPriority only works on Windows and sets process
+-- priority, valid values are: "normal", "above-normal", "high"
+defaultPriority = "high"
+startupDatabaseOptimization = true
+
+-- Status server information
+ownerName = "Qwizer"
+ownerEmail = "onjogos.rs@gmail.com"
+url = "http://www."
+location = "Brasil"
