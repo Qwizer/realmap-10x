a7588391f28dac9916a700472652d20369c2b6ec
Qwizer <romario.sr@live.com>
2017-08-08 12:44:17 -0300

    correção de erros

--- data/globalevents/scripts/autobroadcast.lua
+++ data/globalevents/scripts/autobroadcast.lua
@@ -4,7 +4,7 @@ function onThink(interval, lastExecution)
 	local messages = {
 		"[COMANDOS] !report - !pz - !kills - !bless - !aol - !buyhouse - !sellhouse - !serverinfo - !online - !uptime - !outfit.",
 		"[SORTEIO] Entre agora em nosso facebook ... E participe do nosso sorteio Comartilhando vc pode ganar  1 BOOTS VIP.",
-		"[BUGS] problemas ou sugestões? Entre em nosso site e mande um Ticket!\nA cada Report válido de BUG's você recebe um bonus!"
+		"[BUGS] problemas ou sugestões? Entre em nosso site e mande um Ticket!\nA cada Report válido de BUG's você recebe um bonus!",
 		"[Contato] Skype: onjogos whatsapp: (66) 9 8427-4493!"
 	}
 
